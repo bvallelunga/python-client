0db48bbef83584ff34b696a20faa09458c50ef34
Tom Piccirello <8296030+Piccirello@users.noreply.github.com>
2021-03-03 11:29:51 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import os
+# this is an example API Key and is not valid
 os.environ["DOPPLER_API_KEY"] = "vSNdjLhatIkD8w6F1W0IEDvKfflbTUkRh8AvghaA"
 os.environ["DOPPLER_PIPELINE"] = "31"
 os.environ["DOPPLER_ENVIRONMENT"] = "development_python"
